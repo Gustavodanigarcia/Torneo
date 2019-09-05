d33cd66420b243507141593f040e94fd66914f78
Gustavodanigarcia <52510083+Gustavodanigarcia@users.noreply.github.com>
2019-09-05 18:02:45 -0300

    Update Fecha.java

--- Fecha.java
+++ Fecha.java
@@ -1,48 +1,73 @@
-package ProyectoTorneo.Modelo;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Fecha {
 
 	
-	private int numeroDeFecha;
-	private int año;
-	private List<Partido> partidos = new ArrayList<>();
+private List<Partido> partidos = new ArrayList<>();
 	
-	public Fecha() {
-		
-		
-		
-		
-		
+	private int IdFecha;   //Cada Fecha es unica::::
+	
+	private int IdCampeonato;  //Cada Fecha va a tener un ID campeonato para relacionarlo al campeonato para futuras busquedas:::
+
+
+	//Get y Set de ID Fecha::::
+	public int getIdFecha() {
+		return IdFecha;
+	}
+	
+	public void setIdFecha(int idFecha) {
+		IdFecha = idFecha;
 	}
 	
 	
-	
+	//Get y Set de Todos los Partidos::::
 	public List<Partido> getPartidos() {
 		return partidos;
 	}
-
-
 
 	public void setPartidos(List<Partido> partidos) {
 		this.partidos = partidos;
 	}
 
+//Get y set del ID Campeonato::::
+	public int getIdCampeonato() {
+		return IdCampeonato;
+	}
 
+	public void setIdCampeonato(int idCampeonato) {
+		IdCampeonato = idCampeonato;
+	}
 
-	public int getNumeroDeFecha() {
-		return numeroDeFecha;
+	
+	//Boolean si esta vacia la lista de Partidos::::
+	public boolean isEmpty() {
+		
+				if(this.partidos.isEmpty())
+				return true;
+				else
+					return false;
 	}
-	public void setNumeroDeFecha(int numeroDeFecha) {
-		this.numeroDeFecha = numeroDeFecha;
+	
+	
+	//Add partido en Lista partidos::::
+	public void add(Partido partido) {
+		partidos.add(partido);
 	}
-	public int getAño() {
-		return año;
+	
+	//Comprueba si ya jugaron los mismos equipos en otro partido recorriendo todos los partidos de todas las Fechas:::
+	public boolean yaJugaron(Equipo local, Equipo visitante) {
+		
+		for(Partido partido: partidos) {
+			
+			if(partido.partidoExistente(local, visitante));
+			
+			return true;
+		}		
+			return false;	
 	}
-	public void setAño(int año) {
-		this.año = año;
-	}
+	
+	
+	
 	
 }
